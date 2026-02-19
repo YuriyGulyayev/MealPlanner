ee75b361b2fd4039f199844a99b33ebe1488ccc4
Yuriy Gulyayev <yuriy.gulyayev@gmail.com>
2026-02-18 23:14:50 -0500

    Changed meal plan beginning date. Id=202602184

--- Launchers/MealPlanner.bash
+++ Launchers/MealPlanner.bash
@@ -4,7 +4,7 @@ main()
 {
    set -e
    # clear
-   deno run --cached-only --no-prompt -- ../Src/Main.ts '--beginning-date=2026-02-01' '--number-of-days=1462' > Meal-Plans.txt
+   deno run --cached-only --no-prompt -- ../Src/Main.ts '--beginning-date=2026-01-17' '--number-of-days=1462' > Meal-Plans.txt
 }
 
 main
